935c923b1afee1857c5d7d5cac0a4a69bbc87634
rajenth <rkumar@aidminutes.com>
2023-08-25 08:17:31 +0200

    screen component created

---
+++ formula_x/lib/formula_x_web/components/screen.ex
@@ -0,0 +1,163 @@
+defmodule FormulaXWeb.Screen do
+  use Phoenix.Component
+
+  alias FormulaX.Race.Car
+
+  def render(assigns = %{phase: :off}) do
+    ~H"""
+    <div class="screen">
+    </div>
+    """
+  end
+
+  def render(assigns = %{phase: :startup}) do
+    ~H"""
+    <div class="screen startup_screen">
+      <div class="body">
+        <div class="content">
+          <div class="text_container">
+            <h1><span class="title">Formula</span><span class="title_suffix">X</span></h1>
+            <p class="subtitle">Powered by Elixir/Phoenix</p>
+            <p class="subtitle">Built by Rajenth</p>
+          </div>
+        </div>
+      </div>
+      <div class="footer">
+        <p>Press <span class="green">Green</span> button or <span class="arrow">&#8679</span> key to proceed</p>
+      </div>
+    </div>
+    """
+  end
+
+  def render(assigns = %{phase: :car_selection}) do
+    ~H"""
+    <div class="screen car_selection_screen">
+      <div class="body">
+        <%= with {current_selection_index, _maximum_index, all_available_cars} <- @car_selection_tuple,
+                  car                                                         <- Enum.at(all_available_cars, current_selection_index) do %>
+          <img src={"/images/cars/#{car}"}/>
+        <% end %>
+      </div>
+      <div class="footer">
+        <p>Browse cars using <span class="yellow">Yellow</span> and <span class="blue">Blue</span> buttons or using <span class="arrow">&#8678</span> and <span class="arrow">&#8680</span> keys</p>
+        <p>Press <span class="green">Green</span> button or <span class="arrow">&#8679</span> key to select your car and proceed</p>
+      </div>
+    </div>
+    """
+  end
+
+  def render(assigns = %{phase: :race_info}) do
+    ~H"""
+    <div class="screen race_info_screen">
+      <div class="body">
+        <h1 class="title">Instructions</h1>
+        <ul class="instructions_list">
+          <li>
+            <p>Press <span class="green">Green</span> button or <span class="arrow">&#8679</span> key to start car</p>
+          </li>
+          <li>
+            <p>After car starts use the <span class="green">Green</span> button or <span class="arrow">&#8679</span> key to switch speeds in the increasing order of rest, low, moderate and high</p>
+          </li>
+          <li>
+            <p>Use the <span class="red">Red</span> button or <span class="arrow">&#8681</span> key to switch speeds in the decreasing order of high, moderate, low, rest</p>
+          </li>
+          <li>
+            <p>Use the <span class="yellow">Yellow</span> button or <span class="arrow">&#8678</span> key to move left</p>
+          </li>
+          <li>
+            <p>Use the <span class="blue">Blue</span> button or <span class="arrow">&#8680</span> key to move right</p>
+          </li>
+          <li>
+            <p>Try to navigate the lanes and finish the race</p>
+          </li>
+          <li>
+            <p>Whoever finishes the race in the shortest time wins the race!!!</p>
+          </li>
+        </ul>
+      </div>
+      <div class="footer">
+        <p>Press <span class="green">Green</span> button or <span class="arrow">&#8679</span> key to proceed</p>
+      </div>
+    </div>
+    """
+  end
+
+  def render(assigns = %{phase: :countdown}) do
+    ~H"""
+    <%= # Insert countdown screen in this template and once countdown is over send a message ":start_race" to live view. This template should have race initial visual along with countdown div" %>
+    <div class="screen countdown_screen">
+      <.background images={@race.background.left_side_images} y_position={@race.background.y_position}/>
+      <div class="race">
+        <.lanes/>
+        <.cars cars={@race.cars}/>
+      </div>
+      <.background images={@race.background.right_side_images} y_position={@race.background.y_position}/>
+    </div>
+    """
+  end
+
+  def render(assigns = %{phase: :active_race}) do
+    ~H"""
+    <div class="screen active_race_screen">
+      <.background images={@race.background.left_side_images} y_position={@race.background.y_position}/>
+      <div class="race">
+        <.lanes/>
+        <.cars cars={@race.cars}/>
+      </div>
+      <.background images={@race.background.right_side_images} y_position={@race.background.y_position}/>
+    </div>
+    """
+  end
+
+  def render(assigns = %{phase: :result}) do
+    ~H"""
+    <div class="screen">
+    </div>
+    """
+  end
+
+  defp lanes(assigns) do
+    ~H"""
+    <div class="lanes">
+      <div class="lane"></div>
+      <div class="lane"></div>
+      <div class="lane"></div>
+    </div>
+    """
+  end
+
+  defp cars(assigns) do
+    ~H"""
+    <div class="cars">
+      <%= for car <- @cars do %>
+        <img src={"/images/cars/#{car.image}"} style={car_position_style(car)}/>
+      <% end %>
+    </div>
+    """
+  end
+
+  defp background(assigns) do
+    ~H"""
+    <div class="background" style={background_position_style(@y_position)}>
+      <%= for image <- @images do %>
+        <div class="image_container">
+          <img src={"/images/backgrounds/#{image}"} />
+        </div>
+      <% end %>
+    </div>
+    """
+  end
+
+  @spec car_position_style(Car.t()) :: String.t()
+  defp car_position_style(%Car{
+         x_position: x_position,
+         y_position: y_position
+       }) do
+    "left: #{x_position}px; bottom: #{y_position}px;"
+  end
+
+  @spec background_position_style(Car.y_position()) :: String.t()
+  defp background_position_style(y_position) when is_integer(y_position) do
+    "top: #{y_position}px"
+  end
+end
--- formula_x/lib/formula_x_web/formula_x_live.ex
+++ formula_x/lib/formula_x_web/formula_x_live.ex
@@ -5,9 +5,9 @@ defmodule FormulaXWeb.RaceLive do
   use FormulaXWeb, :live_view
 
   alias FormulaX.Race
-  alias FormulaX.Race.Car
   alias FormulaX.Race.Car.Controls
   alias FormulaX.Race.RaceEngine
+  alias FormulaXWeb.Screen
   alias FormulaX.Utils
 
   @impl true
@@ -16,7 +16,7 @@ defmodule FormulaXWeb.RaceLive do
     <div class="race_live" phx-window-keydown="keydown">
       <div class="console">
         <.speed_controls/>
-        <.screen phase={@phase} race={@race} car_selection_tuple={@car_selection_tuple}/>
+        <Screen.render phase={@phase} race={@race} car_selection_tuple={@car_selection_tuple}/>
         <.direction_controls/>
       </div>
     </div>
@@ -28,151 +28,6 @@ defmodule FormulaXWeb.RaceLive do
     <div class="speed_controls">
       <a class="top" href="#" phx-click="green_button_clicked"></a>
       <a class="bottom" href="#" phx-click="red_button_clicked"></a>
-    </div>
-    """
-  end
-
-  defp screen(assigns = %{phase: :startup}) do
-    ~H"""
-    <div class="screen startup_screen">
-      <div class="body">
-        <div class="content">
-          <div class="text_container">
-            <h1><span class="title">Formula</span><span class="title_suffix">X</span></h1>
-            <p class="subtitle">Powered by Elixir/Phoenix</p>
-            <p class="subtitle">Built by Rajenth</p>
-          </div>
-        </div>
-      </div>
-      <div class="footer">
-        <p>Press <span class="green">Green</span> button or <span class="arrow">&#8679</span> key to proceed</p>
-      </div>
-    </div>
-    """
-  end
-
-  defp screen(assigns = %{phase: :car_selection}) do
-    ~H"""
-    <div class="screen car_selection_screen">
-      <div class="body">
-        <%= with {current_selection_index, _maximum_index, all_available_cars} <- @car_selection_tuple,
-                  car                                                         <- Enum.at(all_available_cars, current_selection_index) do %>
-          <img src={"/images/cars/#{car}"}/>
-        <% end %>
-      </div>
-      <div class="footer">
-        <p>Browse cars using <span class="yellow">Yellow</span> and <span class="blue">Blue</span> buttons or using <span class="arrow">&#8678</span> and <span class="arrow">&#8680</span> keys</p>
-        <p>Press <span class="green">Green</span> button or <span class="arrow">&#8679</span> key to select your car and proceed</p>
-      </div>
-    </div>
-    """
-  end
-
-  defp screen(assigns = %{phase: :race_info}) do
-    ~H"""
-    <div class="screen race_info_screen">
-      <div class="body">
-        <h1 class="title">Instructions</h1>
-        <ul class="instructions_list">
-          <li>
-            <p>Press <span class="green">Green</span> button or <span class="arrow">&#8679</span> key to start car</p>
-          </li>
-          <li>
-            <p>After car starts use the <span class="green">Green</span> button or <span class="arrow">&#8679</span> key to switch speeds in the increasing order of rest, low, moderate and high</p>
-          </li>
-          <li>
-            <p>Use the <span class="red">Red</span> button or <span class="arrow">&#8681</span> key to switch speeds in the decreasing order of high, moderate, low, rest</p>
-          </li>
-          <li>
-            <p>Use the <span class="yellow">Yellow</span> button or <span class="arrow">&#8678</span> key to move left</p>
-          </li>
-          <li>
-            <p>Use the <span class="blue">Blue</span> button or <span class="arrow">&#8680</span> key to move right</p>
-          </li>
-          <li>
-            <p>Try to navigate the lanes and finish the race</p>
-          </li>
-          <li>
-            <p>Whoever finishes the race in the shortest time wins the race!!!</p>
-          </li>
-        </ul>
-      </div>
-      <div class="footer">
-        <p>Press <span class="green">Green</span> button or <span class="arrow">&#8679</span> key to proceed</p>
-      </div>
-    </div>
-    """
-  end
-
-  defp screen(assigns = %{phase: :countdown}) do
-    ~H"""
-    <%= # Insert countdown screen in this template and once countdown is over send a message ":start_race" to live view. This template should have race initial visual along with countdown div" %>
-    <div class="screen countdown_screen">
-      <.background images={@race.background.left_side_images} y_position={@race.background.y_position}/>
-      <div class="race">
-        <.lanes/>
-        <.cars cars={@race.cars}/>
-      </div>
-      <.background images={@race.background.right_side_images} y_position={@race.background.y_position}/>
-    </div>
-    """
-  end
-
-  defp screen(assigns = %{phase: :active_race}) do
-    ~H"""
-    <div class="screen active_race_screen">
-      <.background images={@race.background.left_side_images} y_position={@race.background.y_position}/>
-      <div class="race">
-        <.lanes/>
-        <.cars cars={@race.cars}/>
-      </div>
-      <.background images={@race.background.right_side_images} y_position={@race.background.y_position}/>
-    </div>
-    """
-  end
-
-  defp screen(assigns = %{phase: :result}) do
-    ~H"""
-    <div class="screen">
-    </div>
-    """
-  end
-
-  defp screen(assigns = %{phase: :off}) do
-    ~H"""
-    <div class="screen">
-    </div>
-    """
-  end
-
-  defp lanes(assigns) do
-    ~H"""
-    <div class="lanes">
-      <div class="lane"></div>
-      <div class="lane"></div>
-      <div class="lane"></div>
-    </div>
-    """
-  end
-
-  defp cars(assigns) do
-    ~H"""
-    <div class="cars">
-      <%= for car <- @cars do %>
-        <img src={"/images/cars/#{car.image}"} style={car_position_style(car)}/>
-      <% end %>
-    </div>
-    """
-  end
-
-  defp background(assigns) do
-    ~H"""
-    <div class="background" style={background_position_style(@y_position)}>
-      <%= for image <- @images do %>
-        <div class="image_container">
-          <img src={"/images/backgrounds/#{image}"} />
-        </div>
-      <% end %>
     </div>
     """
   end
@@ -536,19 +391,6 @@ defmodule FormulaXWeb.RaceLive do
     socket = assign(socket, :race, race)
 
     {:noreply, socket}
-  end
-
-  @spec car_position_style(Car.t()) :: String.t()
-  defp car_position_style(%Car{
-         x_position: x_position,
-         y_position: y_position
-       }) do
-    "left: #{x_position}px; bottom: #{y_position}px;"
-  end
-
-  @spec background_position_style(Car.y_position()) :: String.t()
-  defp background_position_style(y_position) when is_integer(y_position) do
-    "top: #{y_position}px"
   end
 
   defp initialise_car_selection_tuple() do
