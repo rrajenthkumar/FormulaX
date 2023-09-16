f5844d3e7616e7f9ce1fbe27e8f019cddc76ded4
rajenth <rkumar@aidminutes.com>
2023-09-16 08:21:16 +0200

    utils code finalised

--- formula_x/lib/formula_x/utils.ex
+++ formula_x/lib/formula_x/utils.ex
@@ -2,13 +2,13 @@ defmodule FormulaX.Utils do
   @moduledoc """
   Module for utility functions
   """
-  @spec get_images(String.t()) :: list(String.t()) | Error.t()
-  def get_images(folder_name) do
+  @spec get_filenames_of_images(String.t()) :: list(String.t()) | Error.t()
+  def get_filenames_of_images(folder_name) do
     folder_path = "#{Application.app_dir(:formula_x)}/priv/static/images/#{folder_name}"
 
     case File.ls(folder_path) do
-      {:ok, available_images} ->
-        available_images
+      {:ok, filenames_of_available_images} ->
+        filenames_of_available_images
 
       {:error, error} ->
         raise error
