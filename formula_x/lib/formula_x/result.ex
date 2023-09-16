cfabd4e4dc2921b2315b9bdaa7e9cf2401b0b5ef
rajenth <rkumar@aidminutes.com>
2023-09-16 09:19:15 +0200

    result code finalised

--- formula_x/lib/formula_x/result.ex
+++ formula_x/lib/formula_x/result.ex
@@ -1,6 +1,6 @@
 defmodule FormulaX.Result do
   @moduledoc """
-  **Result context**
+  Module used to generate results at the end of a race
   """
   use TypedStruct
 
@@ -9,15 +9,14 @@ defmodule FormulaX.Result do
   alias FormulaX.Race.Car
 
   @type status :: :crash | :completed
-  @type review :: :first | :good_start | :bad | :same | :improvement | :decline
 
   @typedoc "Result struct"
   typedstruct do
     field(:car, Car.image(), enforce: true)
-    field(:status, status(), default: nil, enforce: true)
+    field(:status, status(), enforce: true)
     field(:time, Time.t(), default: nil)
     field(:position, integer(), default: nil)
-    field(:review, review(), default: nil)
+    field(:emoji, String.t(), default: nil)
   end
 
   @spec new(map()) :: Result.t()
@@ -27,12 +26,12 @@ defmodule FormulaX.Result do
 
   @spec get_player_car_result(Race.t()) :: Result.t()
   def get_player_car_result(%Race{
-        player_car: %Car{image: player_car_image, controller: :player},
-        status: :crash
+        player_car: %Car{image: image, controller: :player},
+        status: status = :crash
       }) do
     %{
-      car: player_car_image,
-      status: :crash
+      car: image,
+      status: status
     }
     |> Result.new()
   end
@@ -41,30 +40,30 @@ defmodule FormulaX.Result do
         player_car:
           player_car = %Car{
             completion_time: player_car_completion_time,
-            image: player_car_image,
+            image: image,
             controller: :player
           },
         autonomous_cars: autonomous_cars,
         start_time: race_start_time,
-        status: status
+        status: status = :completed
       }) do
     all_cars = autonomous_cars ++ [player_car]
 
-    player_car_index_after_finish =
+    player_car_index_after_sorting_by_completion_time =
       all_cars
       |> Enum.reject(fn car -> is_nil(car.completion_time) end)
       |> Enum.sort_by(& &1.completion_time, Time)
       |> Enum.find_index(fn car -> car.controller == :player end)
 
-    player_car_position = player_car_index_after_finish + 1
+    finishing_position = player_car_index_after_sorting_by_completion_time + 1
 
-    player_car_time_duration = Time.diff(player_car_completion_time, race_start_time, :second)
+    race_time_duration = Time.diff(player_car_completion_time, race_start_time, :second)
 
     %{
-      car: player_car_image,
+      car: image,
       status: status,
-      time: player_car_time_duration,
-      position: player_car_position
+      time: race_time_duration,
+      position: finishing_position
     }
     |> Result.new()
   end
@@ -82,34 +81,42 @@ defmodule FormulaX.Result do
       results_count < 5 ->
         [new_result] ++ last_5_results
     end
-    |> update_review
+    |> add_review
   end
 
-  defp update_review(_last_5_results = [last_result = %Result{}]) do
-    review =
+  @spec add_emoji(list(Result.t())) :: list(Result.t())
+  # Case when the player has played only one race and so there is only one available result
+  defp add_emoji(_last_5_results = [result = %Result{status: status, position: position}]) do
+    emoji =
       cond do
-        last_result.status == :crash -> :bad
-        last_result.position == 1 -> :first
-        true -> :good_start
+        status == :crash -> "&#128555"
+        position == 1 -> "&#127942"
+        true -> "&#128079"
       end
 
-    [%Result{last_result | review: review}]
+    [%Result{result | emoji: emoji}]
   end
 
-  defp update_review(_last_5_results = [last_result = %Result{} | other_results]) do
-    [last_but_one_result | _other_results] = other_results
+  defp add_emoji(
+         _last_5_results = [
+           last_result = %Result{status: last_result_status, position: last_result_position}
+           | other_results
+         ]
+       ) do
+    [last_but_one_result = %Result{position: last_but_one_result_position} | _remaining_results] =
+      other_results
 
-    review =
+    emoji =
       cond do
-        last_result.status == :crash -> :bad
-        last_result.position == 1 -> :first
-        last_result.position < last_but_one_result.position -> :improvement
-        last_result.position > last_but_one_result.position -> :decline
-        last_result.position == last_but_one_result.position -> :same
-        last_but_one_result.position == nil -> :improvement
+        last_result_status == :crash -> "&#128555"
+        last_result_position == 1 -> "&#127942"
+        last_result_position < last_but_one_result_position -> "&#8679"
+        last_result_position > last_but_one_result_position -> "&#8681"
+        last_result_position == last_but_one_result_position -> "&#128528"
+        last_but_one_result_position == nil -> "&#8679"
       end
 
-    updated_last_result = %Result{last_result | review: review}
+    updated_last_result = %Result{last_result | emoji: emoji}
     [updated_last_result] ++ other_results
   end
 end
