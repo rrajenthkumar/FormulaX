10c50d83425d0679027efae8cb5d9f47b78606b6
Rajenth Kumar RAJAKUMAR JEYASINGH <86351645+rrajenthkumar@users.noreply.github.com>
2024-01-18 14:09:22 +0100

    Update dev.exs

--- config/dev.exs
+++ config/dev.exs
@@ -13,7 +13,7 @@ config :formula_x, FormulaXWeb.Endpoint,
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
-  secret_key_base: "dummy_value",
+  secret_key_base: System.fetch_env!("SECRET_KEY_BASE"),
   watchers: [
     esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
     tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
